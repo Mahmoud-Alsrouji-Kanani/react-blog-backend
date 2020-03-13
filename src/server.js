3434182862f7d8accd9b3203a1b9d5be1c9a7156
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2020-03-13 18:38:22 +0400

    Refactoring the code to avoid repeating many lines

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { MongoClient } from 'mongodb';
 const app = express();
 app.use(bodyParser.json());
 
-const withDB =  async (operations) => {
+const withDB =  async (operations, res) => {
     try {  
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }); 
         const db = client.db('react-blog');
@@ -21,18 +21,12 @@ app.get('/api/articles/:name', async (req, res) => {
         const articleName = req.params.name;
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(articleInfo);
-    });
+    }, res);
 });
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
-    try {
+    withDB(async (db) => {
         const articleName = req.params.name;
-    
-        //articlesInfo[articleName].upvotes += 1;
-        //res.status(200).send(`${articleName} no has ${articlesInfo[articleName].upvotes} upvotes.`);
-    
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true }); 
-        const db = client.db('react-blog');
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
@@ -41,10 +35,7 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
         });
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
-        client.close();
-    } catch(error) {
-        res.status(200).json({ message: 'Error connecting to db!', error });
-    }
+    }, res);
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
