22c7b6fc453292a59903215655fcd1bdab5ebe2e
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2020-03-13 20:37:01 +0400

    Serve the frontend

---
+++ src/build/precache-manifest.ae35b1d5d3ca60a00dfde96363bc2360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f26f6fa638bbaf99a69b9d49b86a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe645c81527e296793e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "938b8ce52a629048c0b5",
+    "url": "/static/js/2.cc17fbc6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cc17fbc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe645c81527e296793e",
+    "url": "/static/js/main.63634b85.chunk.js"
+  },
+  {
+    "revision": "f8c88a4ab800e2a20047",
+    "url": "/static/js/runtime-main.ae3b9067.js"
+  }
+]);
